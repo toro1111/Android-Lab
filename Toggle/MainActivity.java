e9be33e6b4aff91e3c42050b394d55383f0c20b6
toro1111 <sonugopi54@gmail.com>
2025-10-08 12:39:31 +0530

    Add files via upload
    toggle

---
+++ Toggle/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView i1,i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1=(ImageView) findViewById(R.id.image1);
+        i2=(ImageView) findViewById(R.id.image2);
+        i1.setOnClickListener(this::onClick);
+        i2.setOnClickListener(this::onClick);
+    }
+    private void onClick(View view){
+        if (view.getId() == R.id.image1){
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.VISIBLE);
+        }
+        else{
+            i2.setVisibility(View.GONE);
+            i1.setVisibility(View.VISIBLE);
+        }
+    }
+}
